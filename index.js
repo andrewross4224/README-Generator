b364128adbe200470fad561f94855e2d9801747b
andrewross4224 <andrew.ross4224@gmail.com>
2023-10-26 10:48:16 -0400

    added all prompts for user input

--- index.js
+++ index.js
@@ -2,11 +2,46 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = ["What is your projects name?", `Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+const questions = ["What is your projects name?",
+    "Provide a short description of your project.",
+    "What are the steps to install your project?",
+    "How should this project be used?",
+    "List contributors, and sources of code for your project:",
+    "Please state how your user can test this project."];
 
-- Why did you build this project?
-- What problem does it solve?
-`, "What are the steps to install your project?", "How should this project be used?",];
+const licenses = [
+    'Academic Free License v3.0',
+    'Apache license 2.0',
+    'Artistic license 2.0',
+    'Boost Software License 1.0',
+    'BSD 2-clause "Simplified" license',
+    'BSD 3-clause "New" or "Revised" license',
+    'BSD 3-clause Clear license',
+    'BSD 4-clause "Original" or "Old" license',
+    'BSD Zero-Clause license',
+    'Creative Commons Zero v1.0 Universal',
+    'Creative Commons Attribution 4.0',
+    'Creative Commons Attribution ShareAlike 4.0',
+    'Educational Community License v2.0',
+    'Eclipse Public License 1.0',
+    'Eclipse Public License 2.0',
+    'European Union Public License 1.1',
+    'GNU Affero General Public License v3.0',
+    'GNU General Public License v2.0',
+    'GNU General Public License v3.0',
+    'GNU Lesser General Public License v2.1',
+    'GNU Lesser General Public License v3.0',
+    'LaTeX Project Public License v1.3c',
+    'Microsoft Public License',
+    'MIT License',
+    'Mozilla Public License 2.0',
+    'Open Software License 3.0',
+    'PostgreSQL License',
+    'SIL Open Font License 1.1',
+    'University of Illinois/NCSA Open Source License',
+    'The Unlicense',
+    'zLib License'
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { }
@@ -33,6 +68,22 @@ function init() {
             type: 'input',
             message: questions[3],
             name: 'usage',
+        },
+        {
+            type: 'input',
+            message: questions[4],
+            name: 'contributors',
+        },
+        {
+            type: 'input',
+            message: questions[5],
+            name: 'testing',
+        },
+        {
+            type: 'list',
+            message: 'Select a license:',
+            name: 'license',
+            choices: [...licenses]
         }
     ]).then((response => console.log(response)))
 }
